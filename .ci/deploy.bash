b508f6b3ed3a859dc1ce090883a87a8accbe2dbd
Knarfux <franck.hild@gmail.com>
2020-07-15 22:14:53 -0400

    Add deploy script

---
+++ .ci/deploy.bash
@@ -0,0 +1,7 @@
+mkdir ffmpeg
+wget -qP ffmpeg -O ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+tar -xf ffmpeg.tar.xz -C ffmpeg --strip-components 1
+PATH=$PATH:$(pwd)/ffmpeg
+echo $PATH
+ffmpeg
+npm run build
