ade044921a4aa51f30e14ef1e065f9b0dfd983ba
Knarfux <franck.hild@gmail.com>
2020-08-29 14:17:51 -0400

    Inprove style and add github link

--- src/components/SocialMedias.js
+++ src/components/SocialMedias.js
@@ -3,7 +3,7 @@ import Link from '@material-ui/core/Link'
 import Grid from '@material-ui/core/Grid'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faTwitter, faTwitch
+  faTwitter, faTwitch, faGithub
 } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
@@ -14,18 +14,23 @@ const SocialMedias = () => <Grid container
   spacing={3}
 >
   <Grid item>
-    <Link target="_blank" href="https://twitter.com/knarfux">
+    <Link  color="secondary" target="_blank" href="https://twitter.com/knarfux">
       <FontAwesomeIcon size="2x" icon={faTwitter} />
     </Link>
   </Grid>
   <Grid item>
-    <Link href="mailto:knarfux@gmail.com">
+    <Link  color="secondary" href="mailto:knarfux@gmail.com">
       <FontAwesomeIcon size="2x" icon={faEnvelope} />
     </Link>
   </Grid>
   <Grid item>
-    <Link target="_blank" href="https://twitch.com/knarfux">
+    <Link  color="secondary" target="_blank" href="https://twitch.com/knarfux">
       <FontAwesomeIcon size="2x" icon={faTwitch} />
+    </Link>
+  </Grid>
+  <Grid item>
+    <Link  color="secondary" target="_blank" href="https://github.com/Hild-Franck">
+      <FontAwesomeIcon size="2x" icon={faGithub} />
     </Link>
   </Grid>
 </Grid>
